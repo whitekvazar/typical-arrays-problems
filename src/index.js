d424851d83c633f7399f3b24faf38c217ebb840a
Kirill Romanovskii <whitekvazar@gmail.com>
2021-02-17 19:35:54 +0300

    feat: complete typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array && array.length > 0 ? Math.min.apply(null, array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array && array.length > 0 ? Math.max.apply(null, array) : 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0) {
+    let sum = 0;
+    array.forEach(element => sum += element);
+    return sum / array.length;
+  }
   return 0;
 }
